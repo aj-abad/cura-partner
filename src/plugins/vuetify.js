a5a737a21792cf794a45431606918993e01c5cf5
AJ <ajabad.dev@gmail.com>
2022-01-24 20:25:08 +0800

    Changed Vuetify options

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,16 +1,23 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
-
+import '@mdi/font/css/materialdesignicons.css'
+import "@fontsource/nunito-sans/200.css";
+import "@fontsource/nunito-sans/300.css";
+import "@fontsource/nunito-sans/400.css";
+import "@fontsource/nunito-sans/600.css";
+import "@fontsource/nunito-sans/700.css";
+import "@fontsource/nunito-sans/800.css";
+import "@fontsource/nunito-sans/900.css";
 Vue.use(Vuetify);
 
 export default new Vuetify({
   theme: {
-      options: {
-        customProperties: true,
-      },
+    options: {
+      customProperties: true,
+    },
     themes: {
       light: {
-        primary: '#007BFF',
+        primary: '#c13e5a',
         secondary: '#424242',
         accent: '#82B1FF',
         error: '#FF5252',
