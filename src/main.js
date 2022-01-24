df5c28cdbefd07a206d8fb96ee4ef0fd57711997
AJ <ajabad.dev@gmail.com>
2022-01-24 21:10:51 +0800

    Added CSS overrides

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import "./assets/main.css"
+import "./assets/overrides.css"
 
 Vue.config.productionTip = false
 
