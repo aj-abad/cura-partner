96e0409e8d086d15d31b75df32081f92c7aec587
AJ <ajabad.dev@gmail.com>
2022-01-24 20:24:58 +0800

    Added some CSS

--- src/main.js
+++ src/main.js
@@ -4,8 +4,7 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import '@mdi/font/css/materialdesignicons.css'
+import "./assets/main.css"
 
 Vue.config.productionTip = false
 
