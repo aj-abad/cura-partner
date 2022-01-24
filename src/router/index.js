5b73427120562ae3aef3ef70fd5e42ed7412b2e2
AJ <ajabad.dev@gmail.com>
2022-01-24 18:59:49 +0800

    Added Electron app sign in page

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import DesktopSignIn from "../views/DesktopSignIn"
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: "/",
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/desktop:signin',
+    name: 'SignIn',
+    component: DesktopSignIn
   }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: 'history',
+  base: process.env.BASE_URL,
 })
 
 export default router
