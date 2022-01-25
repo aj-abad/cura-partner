f9467f6543ead7c09b35cbfdb773e04493cd50cb
AJ <ajabad.dev@gmail.com>
2022-01-25 21:52:20 +0800

    Changed to dark mode

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -12,6 +12,7 @@ Vue.use(Vuetify);
 
 export default new Vuetify({
   theme: {
+    dark: true,
     options: {
       customProperties: true,
     },
@@ -23,8 +24,14 @@ export default new Vuetify({
         error: '#FF5252',
         info: '#2196F3',
         success: '#4CAF50',
-        warning: '#FFC107'
+        warning: '#FFC107',
+        bglight: "#f9f2f7"
       },
+      dark: {
+        primary: '#dd4a71',
+        anchor: '#f77fa4',
+        bgdark: "#231d20"
+      }
     },
   },
 });
