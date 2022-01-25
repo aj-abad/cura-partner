3ac8dff23f811b59f3d7024a0093a6e2eca81a9a
AJ <ajabad.dev@gmail.com>
2022-01-25 21:53:11 +0800

    Updated desktop app bar

--- src/components/DesktopTitleBar.vue
+++ src/components/DesktopTitleBar.vue
@@ -1,23 +1,25 @@
 <template>
-  <div
-    class="bglight"
+  <main
+    class="bgdark"
     id="app-title-bar"
     style="height: 32px; z-index: 100"
     :style="style"
   >
     <div style="flex-grow: 1; -webkit-app-region: drag"></div>
     <div id="close-button">
-      <!-- <v-btn
+      <v-btn
+        tabindex="-1"
+        color="bgdark"
         x-small
         tile
         style="padding: 0; height: 32px; width: 48px"
         elevation="0"
-        v-ripple="{ class: 'red--text' }"
       >
         <v-icon style="transform: scale(0.87)"> mdi-minus </v-icon>
-      </v-btn> -->
+      </v-btn>
       <v-btn
-        color="bglight"
+        tabindex="-1"
+        color="bgdark"
         x-small
         tile
         style="padding: 0; height: 32px; width: 48px"
@@ -27,7 +29,7 @@
         <v-icon style="transform: scale(0.87)"> mdi-close </v-icon>
       </v-btn>
     </div>
-  </div>
+  </main>
 </template>
 
 <script>
