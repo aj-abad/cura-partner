6a3dcca6c23259a2b89ff40792680620468f00da
AJ <ajabad.dev@gmail.com>
2022-02-15 13:35:11 +0800

    Renamed component

--- src/components/Desktop/TitleBar.vue
+++ src/components/Desktop/TitleBar.vue
@@ -5,7 +5,12 @@
     style="height: 32px; z-index: 100"
     :style="style"
   >
-    <div style="flex-grow: 1; -webkit-app-region: drag"></div>
+    <div class="pa-1 px-2 d-flex flex-grow-1" style="-webkit-app-region: drag">
+      <div v-if="false">
+        <span class="font-weight-semibold">Cura for Partners</span>
+        <small style="opacity: 0.75"> v{{ version }} </small>
+      </div>
+    </div>
     <div id="close-button">
       <v-btn
         tabindex="-1"
@@ -14,6 +19,7 @@
         tile
         style="padding: 0; height: 32px; width: 48px"
         elevation="0"
+        @click="$ipc.send('minimize')"
       >
         <v-icon style="transform: scale(0.87)"> mdi-minus </v-icon>
       </v-btn>
@@ -26,7 +32,7 @@
         style="padding: 0; height: 32px; width: 48px"
         elevation="0"
         v-ripple="{ class: 'red--text' }"
-        @click="$ipc.send('close')"
+        @click="$ipc.send(minimizeOnClose ? 'minimize' : 'close')"
       >
         <v-icon style="transform: scale(0.87)"> mdi-close </v-icon>
       </v-btn>
@@ -35,11 +41,18 @@
 </template>
 
 <script>
+import { version } from "../../../package.json";
 export default {
-  name: "DesktopTitleBar",
+  name: "TitleBar",
   computed: {
     isWindowTransparent() {
       return this.$route.meta.isTransparent;
+    },
+    version() {
+      return version;
+    },
+    minimizeOnClose() {
+      return localStorage.getItem("minimizeOnClose") === "true";
     },
     style() {
       return {
@@ -47,6 +60,9 @@ export default {
       };
     },
   },
+  created(){
+    console.log(this.$ipc)
+  }
 };
 </script>
 
