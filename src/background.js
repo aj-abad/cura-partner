243afbe954a6f590a54511a83784e752b467d610
AJ <ajabad.dev@gmail.com>
2022-02-15 13:34:42 +0800

    Added minimize event and preload on main window

--- src/background.js
+++ src/background.js
@@ -1,22 +1,51 @@
-'use strict'
+"use strict";
 
-import { app, protocol, BrowserWindow, ipcMain } from 'electron'
-import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
-import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
-const isDevelopment = process.env.NODE_ENV !== 'production'
+import { app, protocol, BrowserWindow, ipcMain } from "electron";
+import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
+import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+console.log(__dirname + "/preload.js");
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
-  { scheme: 'app', privileges: { secure: true, standard: true } }
-])
-//TODO check if user is logged in on app start
-const token = null
+  { scheme: "app", privileges: { secure: true, standard: true } },
+]);
 
 ipcMain.on("close", (event, arg) => {
-  app.quit()
+  app.quit();
+});
+ipcMain.on("minimize", (event, arg) => {
+  BrowserWindow.getAllWindows().forEach((win) => win.minimize());
+});
 
-})
+async function createStartWindow() {
+  const startWindow = new BrowserWindow({
+    width: 300,
+    height: 350,
+    frame: false,
+    transparent: true,
+    resizable: false,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: __dirname + "/preload.js",
+    },
+  });
+  // startWindow.webContents.openDevTools()
+  startWindow.setMenu(null);
+  startWindow.loadURL(process.env.WEBPACK_DEV_SERVER_URL + "desktop/start");
+  startWindow.on("ready-to-show", () => startWindow.show());
+  startWindow.on("system-context-menu", (event, _point) => {
+    event.preventDefault();
+  });
 
+  ipcMain.on("done-loading", (e, arg) => {
+    const { isLoggedIn } = arg;
+    isLoggedIn ? createMainWindow() : createSignInWindow();
+    startWindow.close()
+  });
+}
 
 async function createSignInWindow() {
   const loginWindow = new BrowserWindow({
@@ -26,94 +55,90 @@ async function createSignInWindow() {
     transparent: true,
     resizable: false,
     webPreferences: {
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
-      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION,
-      preload: __dirname + '/preload.js'
-    }
-  })
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: __dirname + "/preload.js",
+    },
+  });
   // loginWindow.webContents.openDevTools()
-  loginWindow.setMenu(null)
-  loginWindow.loadURL(process.env.WEBPACK_DEV_SERVER_URL + "desktop:signin")
-  loginWindow.on("ready-to-show", () => loginWindow.show())
+  loginWindow.setMenu(null);
+  loginWindow.loadURL(process.env.WEBPACK_DEV_SERVER_URL + "desktop/signin");
+  loginWindow.on("ready-to-show", () => loginWindow.show());
   ipcMain.on("successful-signin", (event, arg) => {
-    loginWindow.close()
-  })
+    loginWindow.close();
+  });
 }
-
 
 async function createMainWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1366,
+    height: 768,
     frame: false,
     webPreferences: {
-
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
-      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION
-    }
-  })
-
-
-
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: __dirname + "/preload.js",
+    },
+  });
+  win.setBackgroundColor("#231d20");
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
-    win.setMenu(null)
-    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+    win.setMenu(null);
+    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
 
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
+    if (!process.env.IS_TEST) win.webContents.openDevTools();
   } else {
-    createProtocol('app')
+    createProtocol("app");
     // Load the index.html when not in development
     // win.loadURL('app://./index.html')
   }
 }
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0)
-    token ? createMainWindow() : createSignInWindow()
-})
+  if (BrowserWindow.getAllWindows().length === 0) createStartWindow();
+});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', async () => {
+app.on("ready", async () => {
   if (isDevelopment && !process.env.IS_TEST) {
     // Install Vue Devtools
     try {
-      await installExtension(VUEJS_DEVTOOLS)
+      await installExtension(VUEJS_DEVTOOLS);
     } catch (e) {
-      console.error('Vue Devtools failed to install:', e.toString())
+      console.error("Vue Devtools failed to install:", e.toString());
     }
   }
-  token ? createMainWindow() : createSignInWindow()
-})
+  createStartWindow();
+});
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
-  if (process.platform === 'win32') {
-    process.on('message', (data) => {
-      if (data === 'graceful-exit') {
-        app.quit()
+  if (process.platform === "win32") {
+    process.on("message", (data) => {
+      if (data === "graceful-exit") {
+        app.quit();
       }
-    })
+    });
   } else {
-    process.on('SIGTERM', () => {
-      app.quit()
-    })
+    process.on("SIGTERM", () => {
+      app.quit();
+    });
   }
 }
