747de42cff3f6ed93c1988e48227ce08230b6058
AJ <ajabad.dev@gmail.com>
2022-02-15 13:36:54 +0800

    Updated routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,44 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import DesktopSignIn from "../views/Desktop/SignIn"
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import DesktopSignIn from "../views/Desktop/SignIn";
+import DesktopStart from "../views/Desktop/Start";
 import isElectron from "is-electron";
-Vue.use(VueRouter)
-
+Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: 'Home',
-    component: Home
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/desktop:signin',
-    name: 'DesktopSignIn',
+    path: "/desktop/start",
+    name: "DesktopStart",
+    component: DesktopStart,
+    meta: {
+      isTransparent: true,
+      isDesktop: true,
+      hideTitleBar: true,
+    },
+  },
+  {
+    path: "/desktop/signin",
+    name: "DesktopSignIn",
     component: DesktopSignIn,
     meta: {
       isTransparent: true,
-      isDesktop: true
-    }
-  }
-]
+      isDesktop: true,
+    },
+  },
+];
 //.filter(route => !isElectron() ? !route.meta?.isDesktop : true)
-
 
 const router = new VueRouter({
   routes,
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  isElectron: isElectron()
-})
+  isElectron: isElectron(),
+});
 
-export default router
+export default router;
