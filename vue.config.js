8f202b4a6c97751a316baa4c0a94ce372a573d68
AJ <ajabad.dev@gmail.com>
2022-02-15 14:06:42 +0800

    Moved preload back to config

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,11 @@ module.exports = {
         return options
       })
   },
+  pluginOptions: {
+    electronBuilder: {
+      preload: 'src/preload.js',
+    }
+  },
   transpileDependencies: [
     'vuetify'
   ]
